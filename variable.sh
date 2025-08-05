74d013dcc8b762cabf2edfe7aedd2737a723475a
venkatakrishnam.aws <vk2860094@gmail.com>
2025-08-05 22:49:24 +0530

    2

---
+++ variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Hello, Ramesh!"
+echo "This is a simple script to demonstrate shell scripting."
+echo "You can modify this script to suit your needs."
+echo "Feel free to add more commands or functionality as required."
+echo "Happy scripting!"
