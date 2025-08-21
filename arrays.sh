6811f7cd4dcf050afe57796144240e1a35b8d5af
venkatakrishnam.aws <vk2860094@gmail.com>
2025-08-21 22:30:05 +0530

    this is my fourth push

--- arrays.sh
+++ arrays.sh
@@ -4,3 +4,4 @@ Fruits=("Apple" "Banana" "Guava" "Mango")
 echo "first fruit is: ${Fruits[0]}"
 echo "Second fruit is: ${Fruits[1]}"
 echo "Third fruit is: ${Fruits[2]}"
+echo "Fourth fruit is: ${Fruits[3]}"
