40c152c4c07ae84605ca8493f23f1c431d085368
venkatakrishnam.aws <vk2860094@gmail.com>
2025-08-21 19:51:08 +0530

    This is my second push

--- 05-variables.sh
+++ 05-variables.sh
@@ -6,6 +6,8 @@ read -s username
 
 echo "Please enter your password:"
 read -s password
+date=$(date)
+time=$(date +%T)
 echo "username is: $username and password is: $password"
 
 echo "Current date and time: $date $time"
