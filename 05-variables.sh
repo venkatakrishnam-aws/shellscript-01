af581c63ebc280b169ded9d777b937057711991b
venkatakrishnam.aws <vk2860094@gmail.com>
2025-08-21 19:41:06 +0530

    This is my second push

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,11 +1,11 @@
 #!/bin /bash
 
-echo "Please enter your $username" 
-read -s $username
+echo "Please enter your username" 
+read -s username
 
 echo "Please enter your password:"
 read -s password
-echo "username is: $username, and password is: $password"
+echo "username is: $username and password is: $password"
 
 echo "Current date and time: $Date"
 
