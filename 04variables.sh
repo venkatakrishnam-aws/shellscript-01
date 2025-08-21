332b4351bd27a056951ecd1a4d91b6815c696989
venkatakrishnam.aws <vk2860094@gmail.com>
2025-08-21 19:10:18 +0530

    This is my second push for the day

--- 04variables.sh
+++ 04variables.sh
@@ -2,5 +2,6 @@
 
 person1=$1
 person2=$2
-echo "$person1:Hello, $person2 "how are you today?"
-echo "$person2:Hi $person1, "I'm doing well, thank you!"
+echo "$person1:Hello, $person2 how are you today?"
+echo "$person2:Hi $person1, I'm doing well, thank you!"
+
