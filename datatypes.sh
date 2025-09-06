6ee71a7d18cfc5fb767b3866081565297db56d8b
venkatakrishnam.aws <vk2860094@gmail.com>
2025-09-06 21:24:14 +0530

    I'm practicing the conditions

--- datatypes.sh
+++ datatypes.sh
@@ -2,9 +2,9 @@
 
 number1=$1
 number2=$2
-add=$(number1+number2)
+add=$(($1+$2))
 add $(200,303)
-echo "The addition of $number1 and $number2 is : $add"
+echo "The addition of $1 and $2 is : $add"
 echo "The script name is : $0"
 echo "The number of arguments passed : $#"
 echo "The arguments passed are : $*"
