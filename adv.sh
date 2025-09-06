0e088e148e0ef5394fc12412b34ac1dd8b429c45
venkatakrishnam.aws <vk2860094@gmail.com>
2025-09-06 15:00:34 +0530

    I'm practicing the conditions

--- adv.sh
+++ adv.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 ID=$(id -u)
-if [ "$ID -ne 0 "] 
+if [ $ID -ne 0 ] 
 then
-    echo "Error:: Please run this script with root access"
+    echo "Error: Please run this script with root access"
+    exit 1
     else 
     echo "Running with root access"
     fi
+
+    yum install mysql-server -y
+    systemctl start mysqld
+    systemctl enable mysqld
+    echo "MySQL server installed and started"
+    
