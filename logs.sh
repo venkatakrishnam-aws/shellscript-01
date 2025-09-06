6393d997ef2655aad4afec344e3090402e98c494
venkatakrishnam.aws <vk2860094@gmail.com>
2025-09-06 19:29:28 +0530

    I'm practicing the conditions

---
+++ logs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+echo "scriptname : $0"
+
+validate() {
+  if [ $1 -ne 0 ]; then
+    echo "Error: $2 ... failed"
+    exit
+  else
+    echo "$2 ... successful"
+  fi
+}
+if [ "$ID" -ne 0 ]
+then
+    echo "Please run as root"
+    exit
+else 
+    echo "Running with root access"
+fi
+
+    yum install mysql -y
+    validate  $? "installing mysql server"  
+    
+    yum install httpd -y
+    validate  $? "installing httpd server" 
