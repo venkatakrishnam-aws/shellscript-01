8ae4ac8164d69daf9cbf78a7544e404b56b9b6e4
venkatakrishnam.aws <vk2860094@gmail.com>
2025-09-06 19:05:04 +0530

    I'm practicing the conditions

--- functions.sh
+++ functions.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
+
 ID=$(id -u)
+
 validate() {
   if [ $1 -ne 0 ]; then
-    echo "Error: $2 failed"
+    echo "Error: $2 ... failed"
     exit 1
   else
-    echo "$2 successful"
+    echo "$2 ... successful"
   fi
 }
 if [ "$ID" -ne 0 ]
 then
     echo "Please run as root"
     exit
-    else "Running with root access"
-    fi
+else "Running with root access"
+fi
 
     yum install mysql -y
-    VALIDATE  $? "install mysql server"  
+    validate  $? "installing mysql server"  
     
     yum install httpd -y
-    VALIDATE  $? "install httpd server" 
+    validate  $? "installing httpd server" 
