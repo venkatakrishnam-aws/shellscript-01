7000de9f64dd32a29f2f0d95bb3c859a19923e63
venkatakrishnam.aws <vk2860094@gmail.com>
2025-09-07 15:52:12 +0530

    first commit

--- installpack.sh
+++ installpack.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 id=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 if [ "$id" -ne 0 ]; then
-    echo "Please run as root"
+    echo -e "$R Error:: Please run as root $N"
     exit
 else
     echo "Running as root"
